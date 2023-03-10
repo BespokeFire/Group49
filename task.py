4e5f1a25894d1011786286cc338d0e3380d650ae
Kayti <kaytipotgieter@gmail.com>
2023-03-10 16:42:26 -0700

    added function my_datetime def and return val test - TEST2 FAILS

--- task.py
+++ task.py
@@ -1,2 +1,5 @@
 def my_func():
     return "Hello World"
+
+def my_datetime(num_sec):
+    return 
--- tests.py
+++ tests.py
@@ -1,11 +1,14 @@
 import unittest
-
+from task import my_datetime 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
 
+    def test2(self):
+        # Checks that returned value is a string
+        self.assertIsInstance(my_datetime(0), str)
 
 if __name__ == '__main__':
     unittest.main()
