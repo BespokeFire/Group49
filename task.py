e0303ffd2da2a480ca7f96cebfa66cb33f7e947b
Kayti <kaytipotgieter@gmail.com>
2023-03-10 17:36:31 -0700

    added functionality so test3 passes

--- task.py
+++ task.py
@@ -2,4 +2,20 @@ def my_func():
     return "Hello World"
 
 def my_datetime(num_sec):
-    return "hello"
+    # set up month, day, year 
+    month = 1 
+    day = 1 
+    year = 1970 
+    
+    # stringify month and day - check if 0 needs to be added 
+    if month < 10:
+        month = "0" + str(month)
+    else:
+        month = str(month)
+
+    if day < 10:
+        day = "0" + str(day)
+    else:
+        day = str(day)
+        
+    return month + "-" + day + "-" + str(year)
