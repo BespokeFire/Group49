8971d070795dd3efb3dbd51d5c0b0c62b73a42f3
Kayti <kaytipotgieter@gmail.com>
2023-03-10 17:20:09 -0700

    added epoch date return test - test fails

--- tests.py
+++ tests.py
@@ -5,11 +5,13 @@ class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
-
+    # --- testing my_datetime function --- #
     def test2(self):
         # Checks that returned value is a string
         self.assertIsInstance(my_datetime(0), str)
-
+        
+    def test3(self):
+        self.assertEqual(my_datetime(0), "01-01-1970")
 
 if __name__ == '__main__':
     unittest.main()
