b2ccdf9c9bf3fb1ade7a5017c2171385503d0c32
Kayti <kaytipotgieter@gmail.com>
2023-03-10 18:22:27 -0700

    removed linting errors

--- task.py
+++ task.py
@@ -1,13 +1,14 @@
 def my_func():
     return "Hello World"
 
+
 def my_datetime(num_sec):
-    # set up month, day, year 
-    month = 1 
-    day = 1 
-    year = 1970 
-    
-    # stringify month and day - check if 0 needs to be added 
+    # set up month, day, year
+    month = 1
+    day = 1
+    year = 1970
+
+    # stringify month and day - check if 0 needs to be added
     if month < 10:
         month = "0" + str(month)
     else:
@@ -17,5 +18,5 @@ def my_datetime(num_sec):
         day = "0" + str(day)
     else:
         day = str(day)
-        
+
     return month + "-" + day + "-" + str(year)
--- tests.py
+++ tests.py
@@ -1,17 +1,20 @@
 import unittest
-from task import my_datetime 
+from task import my_datetime
+
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
+
     # --- testing my_datetime function --- #
     def test2(self):
         # Checks that returned value is a string
         self.assertIsInstance(my_datetime(0), str)
-        
+
     def test3(self):
         self.assertEqual(my_datetime(0), "01-01-1970")
+
 
 if __name__ == '__main__':
     unittest.main()
