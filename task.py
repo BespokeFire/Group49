a51d20124b70e8fe2c5baace14e08ced78eb1c70
Kayti <kaytipotgieter@gmail.com>
2023-03-12 12:22:21 -0600

    Added comments and docstrings

--- task.py
+++ task.py
@@ -6,11 +6,14 @@ def my_func():
 
 
 def my_datetime(num_sec):
+    """ This function takes in an integer representing a number of seconds
+    since the epoch 01-01-1970 and returns the corresponding datetime
+    taking leap years into account"""
+
     # set up month, day, year
     month = 1
     day = 1
     year = 1970
-
     day_secs = 24*60*60
     yr_secs = (365.242189*day_secs)
 
@@ -37,6 +40,7 @@ def my_datetime(num_sec):
     is_lp = is_leap(year)
     up_pst_lp = numLeaps(year)
 
+    # resolve NYE on leap year edge case
     if is_lp:
         up_pst_lp -= 1
 
@@ -79,6 +83,8 @@ def my_datetime(num_sec):
 
 
 def is_leap(year):
+    """ Helper function to my_datetime() that takes in an integer representing
+    a given year and returns True if it is a leap year and False otherwise"""
     leap = False
     if year % 4 == 0:
         leap = True
@@ -88,12 +94,19 @@ def is_leap(year):
 
 
 def numLeaps(year):
+    """ Helper function to my_datetime() that takes in an integer representing
+    a given year and returns the number of leap years that have happened
+    including the given year if it is a leap year"""
     cur = (year // 4) - (year // 100) + (year // 400)
     pst = 477  # 477 represents the number of leap years prior to 1970
     return cur - pst
 
 
 def findMo(is_leap, days):
+    """ Helper function to my_datetime() that takes in a bool representing
+    whether or not it's a leap year and an integer representing
+    the days of the year that have passed and returns the month and day of
+    the year"""
     mo = 0
     if is_leap is False:
         mos = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
--- tests.py
+++ tests.py
@@ -13,34 +13,47 @@ class TestCase(unittest.TestCase):
         self.assertIsInstance(my_datetime(0), str)
 
     def test3(self):
+        # verifies that the epoch is returned correctly
         self.assertEqual(my_datetime(0), "01-01-1970")
 
     def test4(self):
+        # verify is_leap helper returns True correctly
         self.assertTrue(is_leap(1976))
 
     def test5(self):
+        # verify is_leap helper returns False correctly
         self.assertFalse(is_leap(1974))
 
     def test6(self):
+        # verify numLeaps helper returns correct integer
         self.assertEqual(numLeaps(2000), 8)
 
     def test7(self):
+        # verify numLeaps helper returns correct integer when very large
         self.assertEqual(numLeaps(8360), 1550)
 
     def test8(self):
+        # verify findMo helper returns correct tuple
         self.assertEqual(findMo(False, 35), (4, 1))
 
     def test9(self):
+        # verify my_datetime() output matches assignment sample
         self.assertEqual(my_datetime(123456789), "11-29-1973")
 
     def test10(self):
+        # verify my_datetime() output matches assignment sample
         self.assertEqual(my_datetime(9876543210), "12-22-2282")
 
     def test11(self):
+        # verify my_datetime() output matches assignment sample
         self.assertEqual(my_datetime(201653971200), "02-29-8360")
 
     def test12(self):
+        # verify my_datetime() returns correctly on NYE when leap year
         self.assertEqual(my_datetime(9687991520), "12-31-2276")
-    
+
+    # --- end of my_datetime() testing --- #
+
+
 if __name__ == '__main__':
     unittest.main()
