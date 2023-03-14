50b16ce8e6f8d78686fe3a4b0ecc8372871afb8d
Kayti <kaytipotgieter@gmail.com>
2023-03-13 18:13:41 -0600

    fixed some linter errors

--- task.py
+++ task.py
@@ -57,7 +57,8 @@ def to_lower(string):
     """
     converts a string to lowercase
     """
-    return "".join([chr(ord(char) + 32) if 65 <= ord(char) <= 90 else char for char in string])
+    return "".join([chr(ord(char) + 32) if 65 <= ord(char) <= 90 else char
+                    for char in string])
 
 
 def starts_with_sign(string):
